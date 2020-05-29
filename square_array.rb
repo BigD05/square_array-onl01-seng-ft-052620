67952612c5c55c350f11020465fe51f31a156dae
kyle Dornan <kyledornan26@gmail.com>
2020-05-29 11:31:46 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   new_numbers = []
-  array.each do |num|
-    num2 = num **2 
-    new_numbers.push(num2)
+  array.each do |number|
+    number2 = number**2 
+    new_numbers.push(number2)
     
   end
   new_numbers
